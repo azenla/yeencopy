09b97aed01987fb13f354686d02096e9abd9dc28
Alex Zenla <alex@fork.run>
2023-04-08 00:31:10 -0700

    Support for new yeenserve deployment.

--- src/main/kotlin/gay/pizza/yeencopy/main.kt
+++ src/main/kotlin/gay/pizza/yeencopy/main.kt
@@ -1,50 +1,52 @@
 package gay.pizza.yeencopy
 
 import java.net.URI
+import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 import java.nio.file.Path
-import java.security.MessageDigest
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
-import kotlin.io.path.writeBytes
+import kotlin.io.path.name
 import kotlin.system.measureTimeMillis
 
-fun instanceOfDigest(algorithm: String): MessageDigest = MessageDigest.getInstance(algorithm)
-fun MessageDigest.updateAndDigest(bytes: ByteArray): ByteArray = apply { update(bytes) }.digest()
-fun ByteArray.hash(algorithm: String): ByteArray = instanceOfDigest(algorithm).updateAndDigest(this)
-fun ByteArray.md5Hash(): ByteArray = hash("MD5")
-fun ByteArray.toHexString(): String = joinToString("") { String.format("%02x", it) }
+class Yeen(val url: String)
 
-class Yeen(
-  val bytes: ByteArray,
-  val type: String
-) {
-  val key: String = bytes.md5Hash().toHexString()
+fun Yeen.download(httpClient: HttpClient, directory: Path) {
+  val uri = URL(url.replace(" ", "%20")).toURI()
+  val name = Path(uri.path).fileName.name.replace("%20", "_")
+  val file = directory.resolve(name)
+  val request = HttpRequest.newBuilder()
+    .GET().header("User-Agent", "Pizza-YeenCopy/1.0")
+    .uri(uri).build()
+  val response = httpClient.send(request, BodyHandlers.ofFile(file))
+  if (response.statusCode() != 200) {
+    throw RuntimeException("Failed to download yeen at ${url}: status=${response.statusCode()}")
+  }
 }
-
-fun Yeen.fileExtension(): String = when (type) {
-  "image/jpeg" -> "jpg"
-  "image/png" -> "png"
-  "image/gif" -> "gif"
-  else -> "unk"
-}
-
-fun Yeen.write(path: Path): Unit = path.writeBytes(bytes)
 
 class YeenCollection(
   val yeens: Map<String, Yeen>
 )
 
-fun YeenCollection.writeAll(path: Path) {
+fun YeenCollection.downloadAllParallel(httpClient: HttpClient, executor: ScheduledThreadPoolExecutor, path: Path) {
   path.createDirectories()
-  for ((key, yeen) in yeens) {
-    yeen.write(path.resolve("${key}.${yeen.fileExtension()}"))
+  for (yeen in yeens.values) {
+    executor.execute {
+      try {
+        yeen.download(httpClient, path)
+      } catch (e: Exception) {
+        println("Failed to download ${yeen.url}: $e")
+      }
+    }
+  }
+  while (executor.activeCount > 0) {
+    Thread.sleep(1)
   }
 }
 
@@ -53,7 +55,7 @@ class YeenSaturation(private val limit: Int) {
   private val saturation = AtomicInteger(0)
 
   fun add(yeen: Yeen): Boolean {
-    if (yeens.putIfAbsent(yeen.key, yeen) != null) {
+    if (yeens.putIfAbsent(yeen.url, yeen) != null) {
       saturation.incrementAndGet()
     }
     return saturation.get() >= limit
@@ -62,10 +64,9 @@ class YeenSaturation(private val limit: Int) {
   fun collection(): YeenCollection = YeenCollection(yeens)
 }
 
-class YeenCopy(private val url: String, private val parallelTriesCount: Int = 8) : AutoCloseable {
-  private val httpClient: HttpClient = HttpClient.newHttpClient()
-  private val pool = ScheduledThreadPoolExecutor(parallelTriesCount)
-
+class YeenDiscovery(
+  private val httpClient: HttpClient, private val url: String,
+  private val executor: ScheduledThreadPoolExecutor, private val parallelTriesCount: Int) {
   fun copyUntilSaturation(saturationLimit: Int = 100): YeenCollection {
     val saturation = YeenSaturation(saturationLimit)
     while (true) {
@@ -79,14 +80,14 @@ class YeenCopy(private val url: String, private val parallelTriesCount: Int = 8)
   private fun runTriesParallel(saturation: YeenSaturation): Boolean {
     val flag = AtomicBoolean()
     for (i in 1..parallelTriesCount) {
-      pool.execute {
+      executor.execute {
         val yeen = copyOne()
         if (saturation.add(yeen)) {
           flag.set(true)
         }
       }
     }
-    while (pool.activeCount > 0) {
+    while (executor.activeCount > 0) {
       Thread.sleep(1)
     }
     return flag.get()
@@ -97,28 +98,28 @@ class YeenCopy(private val url: String, private val parallelTriesCount: Int = 8)
       .uri(URI.create(url))
       .header("User-Agent", "Pizza-YeenCopy/1.0")
       .build()
-    val response = httpClient.send(request, BodyHandlers.ofByteArray())
+    val response = httpClient.send(request, BodyHandlers.ofString())
     if (response.statusCode() != 200) {
       throw RuntimeException("Status Code: ${response.statusCode()}")
     }
-    return Yeen(
-      bytes = response.body(),
-      type = response.headers()
-        .firstValue("Content-Type")
-        .orElse("image/unknown")
-    )
-  }
-
-  override fun close() {
-    pool.shutdownNow()
+    val body = response.body()
+    val parts = body.split("src=\"")
+    val fullUrl = url + parts[1].split("\"")[0].trim()
+    return Yeen(fullUrl.trim())
   }
 }
 
 fun main(args: Array<String>) {
   val url = args[0]
-  val copier = YeenCopy(url)
+  val httpClient = HttpClient.newHttpClient()
+  val executor = ScheduledThreadPoolExecutor(8)
+  val discovery = YeenDiscovery(httpClient, url, executor, 8)
   val collection: YeenCollection
-  val timeInMillis = measureTimeMillis { copier.use { collection = copier.copyUntilSaturation() } }
-  println("Collected ${collection.yeens.size} yeens in ${timeInMillis / 1000.0} seconds")
-  collection.writeAll(Path("yeens"))
+  val discoverTimeInMillis = measureTimeMillis { collection = discovery.copyUntilSaturation() }
+  println("Discovered ${collection.yeens.size} yeens in ${discoverTimeInMillis / 1000.0} seconds")
+  val downloadedTimeInMillis = measureTimeMillis {
+    collection.downloadAllParallel(httpClient, executor, Path("yeens"))
+  }
+  println("Downloaded ${collection.yeens.size} yeens in ${downloadedTimeInMillis / 1000.0} seconds")
+  executor.shutdown()
 }
